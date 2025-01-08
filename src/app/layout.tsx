f3f79af17b66a8bce88a6520a08b427b360559cd
jborichevskiy <jborichevskiy@gmail.com>
2025-01-08 15:24:09 -0700

    simplify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,35 +6,15 @@ export const metadata: Metadata = {
   title: 'AlloIRL',
   description: 'AlloIRL Progressive Web App',
   manifest: '/manifest.json',
-  icons: {
-    apple: [
-      { url: '/icons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-  },
   appleWebApp: {
     capable: true,
+    statusBarStyle: 'default',
     title: 'AlloIRL',
-    statusBarStyle: 'black-translucent',
-    startupImage: [
-      {
-        url: '/icons/apple-splash-2048-2732.png',
-        media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
-      },
-    ],
   },
-  formatDetection: {
-    telephone: false,
-  },
-  themeColor: '#000000',
   viewport: {
     width: 'device-width',
     initialScale: 1,
     maximumScale: 1,
-    userScalable: false,
-    viewportFit: 'cover',
-  },
-  other: {
-    'mobile-web-app-capable': 'yes',
   },
 }
 
@@ -45,11 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      </head>
       <body>
         {children}
         <ServiceWorkerRegistration />
