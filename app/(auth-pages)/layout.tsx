83bf7d9a8bbeb9967909b1146bfda56a786a05e4
Arthur Tyukayev <github_arthur@tyukayev.com>
2025-02-19 18:35:53 +0400

    Added Suspense boundary for signin page

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,9 +1,15 @@
+import { Suspense } from "react";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-[calc(100vh-8rem)] w-full flex flex-col items-center justify-center">{children}</div>
+    <Suspense>
+      <div className="min-h-[calc(100vh-8rem)] w-full flex flex-col items-center justify-center">
+        {children}
+      </div>
+    </Suspense>
   );
 }
