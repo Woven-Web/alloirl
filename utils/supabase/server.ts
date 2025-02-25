1b628ee3f7030ec44d6bef1abb0bfbb9f6d1316e
jborichevskiy <jborichevskiy@gmail.com>
2025-02-25 15:20:37 -0700

    drop types, invite chaos

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,6 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { Database } from "./database.types";
 
 export const createClient = async () => {
   const cookieStore = await cookies();
